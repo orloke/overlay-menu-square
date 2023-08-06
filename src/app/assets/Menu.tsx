41e9852da1c1cde819f749d741b80a4276391ab8
Junior Dering <juniordering@hotmail.com>
2023-08-06 13:41:16 -0400

    feat: first commit

---
+++ src/app/assets/Menu.tsx
@@ -0,0 +1,24 @@
+interface assetsProps {
+  /* props go here */
+}
+
+export function Menu() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='ionicon'
+      viewBox='0 0 512 512'
+      width={32}
+      height={32}
+    >
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeMiterlimit='10'
+        strokeWidth='32'
+        d='M80 160h352M80 256h352M80 352h352'
+      />
+    </svg>
+  );
+}
